34b2ad855482c96efa505c8e7eec35e0c5af3d3d
Yosem Sweet <yosem.sweet@gmail.com>
2013-02-14 08:57:13 -0800

    [#43910187]Fixed an error with the payment url on production

--- app/assets/javascripts/book/load-courses.js
+++ app/assets/javascripts/book/load-courses.js
@@ -5,6 +5,6 @@ $(function() {
 	$(document).on('click', "#courses .course",  function(e) {
 		e.preventDefault();
   	var selectedCourse = e.currentTarget;
-  	window.window.location = "payment.html?id=" + $(selectedCourse).data("course_id");
+  	window.window.location = "/book/payment?id=" + $(selectedCourse).data("course_id");
 	})
 });
