7ece95aa92cdaa5aed117f12bf3c32359e9f97c2
Yosem Sweet <yosem.sweet@gmail.com>
2013-02-22 23:26:30 -0800

    [#45002927]Added a bit better linking and display for bookings to payments.

--- app/admin/bookings.rb
+++ app/admin/bookings.rb
@@ -1,15 +1,29 @@
 ActiveAdmin.register Booking do
   index do
-	    column :id
-			column "course" do |booking|
-				link_to booking.course.to_s, admin_course_path(booking.course)
-			end
-			column "customer" do |booking|
-				link_to booking.customer.email, admin_customer_path(booking.customer)
-			end
-			column :attendees
-			column "owed" do |b| humanized_money_with_symbol(b.owed) end
-			column "paid" do |b| humanized_money_with_symbol(b.paid) end
-	    default_actions
-	  end
+    column :id
+		column "course" do |booking|
+			link_to booking.course.to_s, admin_course_path(booking.course)
+		end
+		column "customer" do |booking|
+			link_to booking.customer.email, admin_customer_path(booking.customer)
+		end
+		column :attendees
+		column "owed" do |b| humanized_money_with_symbol(b.owed) end
+		column "paid" do |b| humanized_money_with_symbol(b.paid) end
+    default_actions
+  end
+	
+	show do |booking|
+		attributes_table do
+			row :course
+			row :customer do booking.customer.email end
+			row :attendees
+			row :owed do humanized_money_with_symbol(booking.owed) end
+			row :paid do link_to humanized_money_with_symbol(booking.paid), :controller => "payments", :action => "index", 'q[booking_id_eq]' => "#{booking.id}".html_safe end
+			row :created_at
+			row :updated_at
+		end
+		active_admin_comments
+	end
+	
 end
--- app/admin/payments.rb
+++ app/admin/payments.rb
@@ -8,4 +8,20 @@ ActiveAdmin.register Payment do
 		end
 		f.buttons
 	end
+	
+	index do
+    column :id
+		column "booking" do |payment|
+			link_to payment.booking.course.to_s, admin_booking_path(payment.booking)			
+		end
+		column "customer" do |payment|
+			link_to payment.booking.customer.email, admin_customer_path(payment.booking.customer)
+		end
+		column :method
+		column "amount" do |p| humanized_money_with_symbol(p.amount) end
+		column :token
+		column :created_at
+
+    default_actions
+  end
 end
