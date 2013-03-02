7648da9923587dcc6212ef65e4a1d8f180a6373c
Yosem Sweet <yosem.sweet@gmail.com>
2013-03-01 23:46:10 -0800

    [#44675305]Got the progress bar working as you move through the steps

--- app/assets/javascripts/form/nav.js
+++ app/assets/javascripts/form/nav.js
@@ -1,16 +1,28 @@
 (function( $ ){
+	var events = {
+		next : function(navigation) { navigation.trigger('next'); },
+		previous : function(navigation) { navigation.trigger('previous'); }
+	};
+	
+	var methods = {
+		notify : function(navigation) {
+			return navigation.hasClass('next') ? events.next(navigation) : events.previous(navigation);
+		}
+	};
+	
   $.fn.navigation = function( options ) {  
-
+		
     // Create some defaults, extending them with any options that were provided
     var settings = $.extend( {
-      validate : function() { 
+      validate : function(e) { 
 				return Bespeak.activeValidation.validate(this.data('validate'))
 			},
-      success : function() { 
+      success : function(e) { 
 				$(this.data('show')).removeClass('hidden'); $(this.data('hide')).addClass('hidden');
+				methods.notify(this);
 				return this; 
 			},
-			failure : function () { return this; }
+			failure : function (e) { return this; }
     }, options);
 
     return this.each(function(i,element) {        
@@ -24,5 +36,13 @@
 
 $(function() {
 	$('.nav > button.next, .nav > button.previous').navigation();
+	
+	$('.nav > button').on('next', function(e) {
+		var currentProgress = $('#form .intro header .progress .bar.active').last();
+		currentProgress.removeClass('active').next('.bar').addClass('active bar-success');
+	});
+	$('.nav > button').on('previous', function(e) {
+		$('#form .intro header .progress .bar.active').last().removeClass('active bar-success').prev('.bar').addClass('active');
+	});
 });
 
