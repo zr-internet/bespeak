d9eada7ca964d1cbc45798e06d699c6448581309
Yosem Sweet <yosem.sweet@gmail.com>
2013-05-23 00:07:11 -0700

    [#50421453] Sped up queries on the bespeak dashboard page

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -1,6 +1,6 @@
 module ActiveAdmin::ViewsHelper 
 	def revenue_per_month(month_start)
-		courses = Course.search({start_at_gteq: month_start, start_at_lt: month_start + 1.month})
+		courses = Course.includes(:course_type, bookings: [:payments]).search({start_at_gteq: month_start, start_at_lt: month_start + 1.month})
 		revenue = {paid: Money.new(0), owed: Money.new(0)}
 		courses.each do |c|
 			revenue[:paid] += c.total_paid
@@ -10,7 +10,7 @@ module ActiveAdmin::ViewsHelper
 	end
 	
 	def revenue_by_site(site, start_at, end_at)
-		courses = site.courses.search({start_at_gteq: start_at, start_at_lt: end_at})
+		courses = site.courses.includes(:course_type, bookings: [:payments]).search({start_at_gteq: start_at, start_at_lt: end_at})
 		revenue = {paid: Money.new(0), owed: Money.new(0)}
 		courses.each do |c|
 			revenue[:paid] += c.total_paid
